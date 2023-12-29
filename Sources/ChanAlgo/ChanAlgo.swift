58394583ef1e65852339dd9219f52bd6f5c4aa7a
Chandram-Dutta <chandramdutta2004@gmail.com>
2023-12-29 21:14:06 +0530

    fix: fixed gitignore

--- Sources/ChanAlgo/ChanAlgo.swift
+++ Sources/ChanAlgo/ChanAlgo.swift
@@ -1,22 +1,21 @@
-class ChanAlgo {
-	
-	func insertionSort<T: Comparable>(_ array: inout [T], increasing: Bool) {
+public extension Array where Element: Comparable {
+	mutating func insertionSort(increasing: Bool = true) {
 		if increasing {
-			var i = array.count
+			var i = count
 			while i != 0 {
 				for j in 1..<i {
-					if array[j - 1] > array[j] {
-						array.swapAt(j, j - 1)
+					if self[j - 1] > self[j] {
+						swapAt(j, j - 1)
 					}
 				}
 				i -= 1
 			}
 		} else {
-			var i = array.count - 1
+			var i = count - 1
 			while i > 0 {
 				for j in 1...i {
-					if array[j - 1] < array[j] {
-						array.swapAt(j, j - 1)
+					if self[j - 1] < self[j] {
+						swapAt(j, j - 1)
 					}
 				}
 				i -= 1
--- Tests/ChanAlgoTests/ChanAlgoTests.swift
+++ Tests/ChanAlgoTests/ChanAlgoTests.swift
@@ -1,84 +1,84 @@
 import XCTest
+import ChanAlgo
 
-@testable import ChanAlgo
-
-final class ChanAlgoTests: XCTestCase {
-  let algo = ChanAlgo()
-  // Test for increasing order sorting
-  func test_insertionSort_increasing() throws {
-    // Arrange
-    var testArray = [5, 2, 4, 6, 1, 3]
-    let expectedResult = [1, 2, 3, 4, 5, 6]
-
-    // Act
-    algo.insertionSort(&testArray, increasing: true)
-
-    // Assert
-    XCTAssertEqual(testArray, expectedResult)
-  }
-
-  // Test for decreasing order sorting
-  func test_insertionSort_decreasing() throws {
-    // Arrange
-    var testArray = [5, 2, 4, 6, 1, 3]
-    let expectedResult = [6, 5, 4, 3, 2, 1]
-
-    // Act
-    algo.insertionSort(&testArray, increasing: false)
-
-    // Assert
-    XCTAssertEqual(testArray, expectedResult)
-  }
-
-  // Test for empty array
-  func test_insertionSort_emptyArray() throws {
-    // Arrange
-    var testArray: [Int] = []
-    let expectedResult: [Int] = []
-
-    // Act
-    algo.insertionSort(&testArray, increasing: true)
-
-    // Assert
-    XCTAssertEqual(testArray, expectedResult)
-  }
-
-  // Test for already sorted array
-  func test_insertionSort_alreadySorted() throws {
-    // Arrange
-    var testArray = [1, 2, 3, 4, 5, 6]
-    let expectedResult = [1, 2, 3, 4, 5, 6]
-
-    // Act
-    algo.insertionSort(&testArray, increasing: true)
-
-    // Assert
-    XCTAssertEqual(testArray, expectedResult)
-  }
-
-  // Test for single-element array
-  func test_insertionSort_singleElement() throws {
-    // Arrange
-    var testArray = [5]
-    let expectedResult = [5]
-
-    // Act
-    algo.insertionSort(&testArray, increasing: true)
-
-    // Assert
-    XCTAssertEqual(testArray, expectedResult)
-  }
-
-  // Test for array with repeating elements
-  func test_insertionSort_repeatingElements() throws {
-    // Arrange
-    var testArray = [5, 2, 2, 4, 4, 1, 3]
-    let expectedResult = [1, 2, 2, 3, 4, 4, 5]
-
-    // Act
-    algo.insertionSort(&testArray, increasing: true)
-
-    // Assert
-    XCTAssertEqual(testArray, expectedResult)
-  }
+final class ArrayInsertionSortTests: XCTestCase {
+	// Test for increasing order sorting
+	func test_insertionSort_increasing() {
+		// Arrange
+		var testArray = [5, 2, 4, 6, 1, 3]
+		let expectedResult = [1, 2, 3, 4, 5, 6]
+		
+		// Act
+		testArray.insertionSort()
+		
+		// Assert
+		XCTAssertEqual(testArray, expectedResult)
+	}
+	
+	// Test for decreasing order sorting
+	func test_insertionSort_decreasing() {
+		// Arrange
+		var testArray = [5, 2, 4, 6, 1, 3]
+		let expectedResult = [6, 5, 4, 3, 2, 1]
+		
+		// Act
+		testArray.insertionSort(increasing: false)
+		
+		// Assert
+		XCTAssertEqual(testArray, expectedResult)
+	}
+	
+	// Edge cases:
+	
+	// Test for empty array
+	func test_insertionSort_emptyArray() {
+		// Arrange
+		var testArray: [Int] = []
+		let expectedResult: [Int] = []
+		
+		// Act
+		testArray.insertionSort()
+		
+		// Assert
+		XCTAssertEqual(testArray, expectedResult)
+	}
+	
+	// Test for already sorted array
+	func test_insertionSort_alreadySorted() {
+		// Arrange
+		var testArray = [1, 2, 3, 4, 5, 6]
+		let expectedResult = [1, 2, 3, 4, 5, 6]
+		
+		// Act
+		testArray.insertionSort()
+		
+		// Assert
+		XCTAssertEqual(testArray, expectedResult)
+	}
+	
+	// Test for single-element array
+	func test_insertionSort_singleElement() {
+		// Arrange
+		var testArray = [5]
+		let expectedResult = [5]
+		
+		// Act
+		testArray.insertionSort()
+		
+		// Assert
+		XCTAssertEqual(testArray, expectedResult)
+	}
+	
+	// Test for array with repeating elements
+	func test_insertionSort_repeatingElements() {
+		// Arrange
+		var testArray = [5, 2, 2, 4, 4, 1, 3]
+		let expectedResult = [1, 2, 2, 3, 4, 4, 5]
+		
+		// Act
+		testArray.insertionSort()
+		
+		// Assert
+		XCTAssertEqual(testArray, expectedResult)
+	}
 }
